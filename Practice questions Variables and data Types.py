95840516d721b975008e4ae08e9c27482cc8cbb7
vamshisai815 <vsai16988@gmail.com>
2026-02-20 20:12:47 +0530

    Add files via upload

---
+++ Practice questions Variables and data Types.py
@@ -0,0 +1,122 @@
+# personal Details 
+# create variables to store 
+# your name 
+# Your age 
+# Your city 
+
+
+name = "Vamshi Sai"
+age = 18
+city = "Bilaspur Chhattisgarh "
+
+
+print("My name is" ,name)
+print ("I am a" ,age)
+print ("I Live in a", city)
+
+#2 Data type Check 
+# Create the following Variables and Check their Data tyoes using type()
+
+X = 50
+area = 45.75
+favorite = "Python Programming"
+Result = True 
+
+print ( "The data type of x is", type(X))
+print (" The data type of area is " , type(area))
+print("The data type of favorite is ", type(favorite))
+print(" The data type of result is ", type(Result) )
+ 
+ 
+ #3 store marks of 3 subject And print their sum 
+
+Businesslaw= 79
+Accounts = 98
+Economics = 90
+
+total = Businesslaw + Accounts + Economics 
+
+print(total)
+
+#4 Monthly Income
+#create variable for- 
+# salary 
+# Bonus 
+
+
+Salary = 30000
+Bonus = 20000 
+TotalIncome = Salary + Bonus 
+print(TotalIncome)
+
+
+#5 create a variable that stores wheather an student has passed or faiked and print it 
+
+Passed = input("Enter weather the student is passed:") 
+Failed = input ("Enter Weather the student is failed:")
+
+print("Enter Weather the student is passed", Passed)
+print("Enter Weather the student is failed", Failed)
+
+#6 strings + Numbers 
+# create a variable age = 18
+# convert it into string and print its data typ  
+
+age= 18
+age = float(age)
+
+print(age)
+print(type(age))
+ 
+ #7 Strings to integer 
+ # Create a Variable -:
+ 
+ #Marks = "85"
+ # converts it into integer and print the result 
+
+marks =  "85"
+marks = int(marks)
+
+print(marks)
+print(type(marks))
+
+#8 Simple Interest 
+# Create variable for ;-
+# principal
+# Rate
+# Time
+# Calculate and print Simple interest
+
+Principal = 30000
+rate = 10
+time = 5  
+SimpleInterst = Principal * rate * time / 100
+ 
+print(SimpleInterst) 
+
+# 9 Student ID Card
+# Create variable for 
+# Studnet Name 
+# Roll Number
+# Percentage
+# IS student present (True/False)
+
+StudentName = "Vamshi Sai"
+RollNumber = 200
+percentage = 69.7
+IsStudentpresent = True 
+
+print(StudentName, RollNumber, percentage , IsStudentpresent )
+
+
+#10 Find the error 
+#Identify and correct the mistake in this code 
+age = int(age)
+total_age = age + 5
+
+
+
+age = int(age)
+
+total_age = age + 5
+print(total_age)
